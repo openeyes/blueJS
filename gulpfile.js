71043a9e83fd9184043e16b2296afe6f89859ca7
Toby Fisher <toby@knowego.com>
2019-12-22 09:42:18 +0000

    Setting up Gulp

---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+/*
+blueJS OE UI JS (JS) - Gulp generates:
+- blueJS.min.js
+- blueJS.idg.js	// read-able + de-bugging 
+*/
+
+const config = {
+	jsPrefix:	'blueJS',
+}
+
+const paths = {
+	js: {
+		input:	'./src/js/**/*.js',
+		output:	'./dist/',
+	},
+	reload:		'./dist/'
+};
+
+/*
+Packages
+*/
+const {gulp, src, dest, watch, series, parallel} = require('gulp');
+const del = require('del');
+const rename = require('gulp-rename');
+const header = require('gulp-header');
+
+// JS
+const jshint = require('gulp-jshint');
+const stylish = require('jshint-stylish');
+const concat = require('gulp-concat');
+const uglify = require('gulp-terser');
+const optimizejs = require('gulp-optimize-js');
+
+/*
+-----------------------------
+Process and JS files
+-----------------------------
+*/
+
+// Lint, minify, and concatenate scripts
+var buildScripts = function (done) {
+	return src(paths.js.input)
+		.pipe(concat(config.jsPrefix + '.js'))
+		.pipe(optimizejs())
+		.pipe(dest(paths.js.output))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(optimizejs())
+		.pipe(dest(paths.js.output));
+
+};
+
+// Lint scripts
+var lintScripts = function (done) {
+	// Lint scripts
+	return src(paths.js.input)
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish'));
+
+};
+
+
+/*
+-----------------------------
+Task helpers
+-----------------------------
+*/
+var cleanDist = function (done) {
+	// Clean the dist folder
+	del.sync([ paths.js.output ]);
+	// Signal completion
+	done();
+};
+
+
+var watchJS = function(done){
+	watch(paths.js.input, series(exports.buildJS));
+	done();
+}
+
+/*
+-----------------------------
+Export Tasks
+-----------------------------
+*/
+exports.buildJS = parallel(buildScripts,lintScripts);
+
+exports.default = series(
+	cleanDist,
+	exports.buildJS
+	//watchJS
+);
