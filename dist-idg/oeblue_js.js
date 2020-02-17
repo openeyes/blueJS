c3ac9a8cf0d2a19e264a49db04b9860854e4260f
Toby Fisher <toby@knowego.com>
2020-02-17 22:53:46 +0000

    sideNavSelectList filter lists from sideNav area

--- dist-idg/oeblue_js.js
+++ dist-idg/oeblue_js.js
@@ -2585,6 +2585,68 @@ const bluejay = (function () {
 
 	'use strict';	
 	
+	uiApp.addModule('sideNavSelectList');	
+	
+	/*
+	Side Nav Select List filtering
+	Used in Worklist, Messages, Trials, etc
+	Anywhere you have a bunch of "lists" that need filtering. 
+	*/
+	
+	const listFilters = uiApp.nodeArray(document.querySelectorAll('.js-list-filter'));
+	if(listFilters.length == 0) return;
+	
+	const allLists = uiApp.nodeArray(document.querySelectorAll('.js-filter-group'));
+	
+	// 'All' should always be selected by default	
+	let activeFilter = document.querySelector('.js-list-filter.selected'); 
+	
+	/**
+	* update the lists shown
+	* @param {string} listID 
+	*/
+	const updateListView = (listID) => {
+		if(listID == "all"){
+			allListsDisplay('block');
+		} else {
+			allListsDisplay('none');
+			document.querySelector('#'+listID).style.display = "block";
+		}
+	};
+	
+	/**
+	* Change all the list display
+	* @param {string} display - 'block' or 'none'
+	*/
+	const allListsDisplay = (display) => {
+		allLists.forEach((list) => {
+			list.style.display = display; 
+		});
+	};	
+	
+	/**	
+	Not using delagation here because each filter
+	link in the sideNav list is an <a>, easier to 
+	handle Event here as we need to preventDefault
+	*/
+	listFilters.forEach((filter) => {
+		filter.addEventListener('click', (ev) => {
+			ev.preventDefault();
+			let link = ev.target;
+			link.classList.add('selected');
+			updateListView(link.dataset.list);
+			activeFilter.classList.remove('selected');
+			activeFilter = link;
+			
+		});
+	});
+ 		
+
+})(bluejay); 
+(function (uiApp) {
+
+	'use strict';	
+	
 	uiApp.addModule('sidebarQuicklookView');
 	
 	/*
