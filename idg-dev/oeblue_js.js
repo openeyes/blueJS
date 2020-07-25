f640ac5511aa7cf4a6b8533587081790b1fddaa0
Toby Fisher <toby@knowego.com>
2020-07-25 16:38:01 +0100

    demo Login Timeout Overlay

--- idg-dev/oeblue_js.js
+++ idg-dev/oeblue_js.js
@@ -1088,6 +1088,37 @@ const bluejay = (function () {
 
 	'use strict';
 	
+	const demo = () => {
+		const div = document.createElement('div');
+		div.className = "oe-popup-wrap dark";
+		document.body.appendChild( div );
+		
+		const template = [
+			'<div class="oe-login timeout">',
+			'<div class="login">',
+			'<h1>Timed out</h1>',
+			'<div class="user">',
+			'<input type="text" placeholder="Username">',
+			'<input type="password" placeholder="Password">',
+			'<button class="green hint" id="js-login">Login</button>',
+			'</div>',
+			'<div class="info">',
+			'For security reasons you have been logged out. Please login again',
+			'</div>',
+			'</div>',
+			'</div>'].join('');
+			
+		div.innerHTML = Mustache.render( template, {} );
+		
+	};
+
+	bluejay.demoLoginTimeOut = demo;	
+			
+})( bluejay ); 
+(function (uiApp) {
+
+	'use strict';
+	
 	/*
 	IDG DEMO of some UX functionality
 	*/
