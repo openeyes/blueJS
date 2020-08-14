c631f7e61b431422ec3f87068c92bb0099563f0d
Toby Fisher <toby@knowego.com>
2020-08-14 12:39:17 +0100

    CopyToClipboard module added

--- idg-dev/oeblue_js.js
+++ idg-dev/oeblue_js.js
@@ -4184,35 +4184,88 @@ const oePlotly = (function ( bj ) {
 	bj.addModule('copyToClipboard');
 	
 	/**
-	"js-copy-to-clipboard"
-	new 
-	js-copytext-to-clipboard
+	current: "js-copy-to-clipboard"
+	new: "js-copytext-to-clipboard"
 	*/
 	
+	const selector = '.js-copytext-to-clipboard';
 	
-	const copy = ( ev ) => {
-		console.log( ev.target.textContent);
-	}
+	if( document.querySelector( '.js-copytext-to-clipboard' ) === null ) return;
 	
+	/*
+	Added to the hospital number and nhs number, provide a UI clue	
+	*/
+	const all = bj.nodeArray( document.querySelectorAll( selector ));
+	all.forEach( ( elem )=>{
+		elem.style.cursor = "pointer";	
+	});
 	
 	/**
-	* Use the clipboard API (if available)
-	* @param {String} str
+	* Copy success!
+	* @param {Element} elem - <span>
 	*/
-	const copyToClipboard = ( str ) => {
+	const success = ( elem ) => {
 		
-		if( !navigator.clipboard) return;
+		let domRect = elem.getBoundingClientRect();
+		let center = domRect.right - (domRect.width/2);
+		let top = domRect.top - 5;
+		let tipHeight = 30;
 		
-		// returns a Promise
-		navigator.clipboard.writeText( str ).then(
-			() => {
-				success();
-			}, () => {
-				failed();
-		});
-	}
+		const div = document.createElement( 'div' );
+		div.className = "oe-tooltip fade-out";
+		div.innerHTML = 'Copied';
+		div.style.width = '80px'; // overide the newblue CSS
+		div.style.top = ( top - tipHeight )+ 'px';
+		div.style.left = ( center - 40 ) + 'px';
+		bj.appendTo('body', div);
+		
+		setTimeout(() => bj.removeElement( div ) , 2500 ); // CSS fade out takes 2 secs.
+	};
 	
-	bj.userDown('.js-copytext-to-clipboard', copy )
+
+	/**
+	* Use the clipboard API (if available)
+	* @param {Element} elem - <span>
+	*/
+	const copyToClipboard = ( elem ) => {
+		/*
+		Note that the API only works when served over secured domains (https) 
+		or localhost and when the page is the browser's currently active tab.
+		*/
+		if(navigator.clipboard){
+			// if availabld use ASYNC new API
+			navigator.clipboard.writeText( elem.textContent )
+				.then(() => {
+					bj.log('[ASYNC] copied text to clipboard');
+					success( elem );
+				})
+				.catch(err => {
+					bj.log('failed to copy text to clipboard');
+				});
+		} else {
+			// or use the old skool method
+			const input = document.createElement('input');
+			input.value = elem.textContent;
+			input.style.position = "absolute";
+			input.style.top = '-200px';
+			
+			document.body.appendChild( input );
+			
+			input.select();
+	
+			document.execCommand("copy");
+			
+			success( elem );
+			
+			// clean up DOM
+			bj.removeElement( input );
+		}		
+	};
+	
+	
+	bj.userDown('.js-copytext-to-clipboard', ( ev ) => {
+		copyToClipboard( ev.target );
+	});
 		
 		
 })( bluejay ); 
