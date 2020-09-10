024c4b7741792927513bf92f30143185b1c4b551
Toby Fisher <toby@knowego.com>
2020-09-10 14:35:12 +0100

    Quick tag improvements and added in UI flag

--- src/idg/quicktag.js
+++ src/idg/quicktag.js
@@ -9,7 +9,7 @@
 	Model
 	*/
 	const model = {
-		qtags: [ 'Apple', 'Avocado', 'Banana', 'Cucumber', 'Coconut', 'Datix', 'Donut', 'Elephant', 'Frisbee', 'Research', 'Teaching', 'Referred', 'Results', 'Results_pending' ].map( t => t.toLowerCase()), // for string matching remove case.
+		qtags: [ 'Apple', 'Avocado', 'Banana', 'Cucumber', 'Coconut', 'Datix', 'Donut', 'Elephant', 'Frisbee', 'Note', 'Research', 'Teaching', 'Referred', 'Results', 'Results_pending' ].map( t => t.toLowerCase()), // for string matching remove case.
 		
 		// input can be either an <input> or <textarea>
 		// but must be 'tagged' for use with: "use-qtags"
@@ -24,12 +24,40 @@
 		// Mustache
 		const template = '{{#qtags}}<span class="qtag">#{{.}}</span> {{/qtags}}';
 		
-		let activeInput = null;
-		let div = null;
+		let ready = false;
 		let tag = "";
 		
+		const elem = {
+			input: null,
+			tags: null,
+			flag: null, 
+		};
+		
 		/**
-		* selectedTag, make available to the controller
+		* Remove qTags
+		*/
+		const remove = () => {
+			bj.remove( elem.tags );
+			bj.remove( elem.flag );
+			bj.unwrap( elem.input );
+			
+			elem.tags = null;
+			elem.flag = null;
+			elem.input = null;
+			
+			ready = false;
+ 		};
+		
+		/**
+		* Show tag swam, hide flag
+		*/
+		const showTags = () => {
+			elem.tags.style.display = "block";
+			elem.flag.style.display = "none";
+		};
+		
+		/**
+		* selectedTag, make this available to the controller
 		* @returns {String || null}
 		*/
 		const selectedTag = () => tag;
@@ -46,11 +74,11 @@
 				so always make the first tag as selected
 				*/
 				tag = tagArr[0];
-				div.innerHTML = Mustache.render( template, { qtags: tagArr });
-				div.querySelector('.qtag:first-child').classList.add('selected'); 
+				elem.tags.innerHTML = Mustache.render( template, { qtags: tagArr });
+				elem.tags.querySelector('.qtag:first-child').classList.add('selected'); 
 			} else {
 				tag = "";
-				div.innerHTML = '<div class="no-matching-tags">No matching Patient Tags</div>';
+				elem.tags.innerHTML = '<div class="no-matching-tags">No matching Patient Tags</div>';
 			}
 		};
 		
@@ -71,14 +99,19 @@
 		};
 		
 		/**
-		* Show the qTags swarm
+		* Intialise on the input
+		* Setup and provide a UI Flag on the input to let the user 
+		* know that they can qtag this input
 		* @param {Element} input - input or textarea to tag;
 		*/
-		const show = ( input ) => {
-			activeInput = input;
+		const init = ( input ) => {
+			if( ready ) return; 
+			
+			ready = true;	
+			elem.input = input;
 			/*
 			Using the "wrap" we can now position the div
-			relatively to the wrapper. The reason behind this
+			relativelto the wrapper. The reason behind this
 			approach (rather than used "fixed") is because of 
 			comments in hotlist. Using fixed causes the "mouseleave"
 			to fire, closing the hotlist panel.  
@@ -88,41 +121,39 @@
 			// "wrap" will cause focus to be lost from the input
 			input.focus();
 			
-			// add qtags selection
-			div = document.createElement('div');
-			div.className = "oe-qtags";
+			// add qtags div (hidden for now)
+			elem.tags = document.createElement('div');
+			elem.tags.className = "oe-qtags";
+			elem.tags.style.display = "none"; 
+			
+			qtags( model.qtags );  // ready, with all tags
 			
 			// check input isn't too close to top of the page
-			const rect = input.getBoundingClientRect();
-			if( rect.top < 150 ){
-				div.style.top = '100%';
+			// note: tag swarm updates as user types and textarea expands down
+			if( input.getBoundingClientRect().top < 150 ){
+				elem.tags.style.top = '100%';
 			} else {
-				div.style.bottom = '100%'; // tag swarm updates as user types, also textarea expands down
+				elem.tags.style.bottom = '100%'; 
 			}
 			
-			// init with all available tags.
-			qtags( model.qtags ); 
+			// UI flag to user
+			elem.flag = document.createElement('div');
+			elem.flag.className = "oe-qtags-flag";
+			elem.flag.textContent = "#";
 			
 			// update DOM
-			wrap.appendChild( div ); 
+			wrap.appendChild( elem.flag );
+			wrap.appendChild( elem.tags ); 
+			
 		};
-		
-		/**
-		* Remove qTags
-		*/
-		const remove = () => {
-			bj.remove( div );
-			bj.unwrap( activeInput );
-			div = null;
-			activeInput = null;
- 		};
 		
 		// reveal
 		return { 
-			show, 
+			init,
+			showTags,
 			userTyping, 
+			selectedTag,
 			remove, 
-			selectedTag, 
 		};
 			
 	})();
@@ -150,7 +181,7 @@
 		if( ev.key == ' ' || ev.key == 'Enter' || ev.key == 'Tab' ){
 			ev.preventDefault();
 			ev.stopPropagation();
-			inputController.useSelectedTag();
+			inputController.insertTag( quickTags.selectedTag());
 		} 
 	};
 	
@@ -166,17 +197,10 @@
 		let inputText = "";
 		
 		/**
-		* Reset - full reset
-		*/
-		const reset = () => {
-			ready();
-			input = null;
-		};
-		
-		/**
 		* Ready to quick tag
+		* and: Callback on the qTag close btn
 		*/
-		const ready = () => {
+		const cancelTagging = () => {
 			tagging = false;
 			insertIndex = 0;
 			quickTags.remove();
@@ -187,76 +211,91 @@
 		* Start quick tagging
 		* @param {Number} tagIndex - character position (selectionStart)
 		*/
-		const start = ( tagIndex ) => {
+		const startTagging = ( tagIndex ) => {
 			// setup for tagging
 			tagging = true;
 			insertIndex = tagIndex;
 			inputText = input.value;
-			quickTags.show( input );
+			quickTags.showTags();
 			document.addEventListener("keydown", handleKeyDown, true ); // Important. Must intercept all other key events first
 		};
 		
 		/**
-		* qTags creates a wrapper to use for positioning. 
-		* building this causes the input to lose focus.
+		* refocus on input.
+		* qTags creates a wrapper to use for positioning 
+		* and use may click the flag both cause the input to lose focus.
 		*/
 		const refocusInput = () => {
 			setTimeout(() => input.focus(), 20);
 		};
 		
 		/**
-		* Insert selected tag from Quick Tags
-		* @param {String} tag - currently selected tag, or empty string
+		* Callback from 'keydown' or .qtag 'mousedown'
+		* @param {String} tag - without '#'
 		*/
-		const insertTagStr = ( tag ) => {
+		const insertTag = ( tag ) => {	
 			let str = tag + ' ';
-			// check for position of insertion
 			if( insertIndex === inputText.length ){
+				// add to the end
 				input.value = inputText + str;
 			} else {
+				// insert in the middle
 				input.value = inputText.substring( 0, insertIndex ) + str + inputText.substring( insertIndex + 1 );
-			}	
+			}
+			
+			cancelTagging();
+			refocusInput();
+		};   
+		
+		/**
+		* Callback: User clicks # UI flag button
+		*/ 
+		const userClicksFlag = ( ev ) => {
+			input.value = input.value + ' #';
+			startTagging( input.value.length );
+			refocusInput();
 		};
 		
-		const useSelectedTag = () => {	
-			insertTagStr( quickTags.selectedTag() );
-			ready();
-			refocusInput();
-		}; 
-		
-		const userClicksTag = ( ev ) => {
-			const tagStr = ev.target.textContent;
-			insertTagStr( tagStr.substring(1) );
-			ready();
-			refocusInput();
-		};  
-		
+		/**	
+		* Callback 'input' event
+		* watching ALL input events, using Event Delegation
+		* note: user could TAB to get to this input
+		* @param {Event} ev - input (textarea, input or select)
+		*/
 		const watch = ( ev ) => {
 			//  has the user switched inputs?
-			if( ev.target !== input && tagging ) reset();
+			if( ev.target !== input && tagging ){
+				input = null;
+				cancelTagging();
+			}
 			
-			// is input allowed to use tags?
-			if( !ev.target.classList.contains( model.inputSelector )) return; // no.
-			
+			// check this input is allowed to use tags...
+			if( !ev.target.classList.contains( model.inputSelector) ) return; // not allowed.
+		
 			/*
-			Either input is tagging or we are watching for "#"
+			Accepted input!
+			Either user is tagging or we are watching for "#" key to activate qTags
 			*/
+			input = ev.target;
+			// highlight to the used that this input has tagging available
+			quickTags.init( input ); 
+			
 			if( tagging ){
-				quickTags.userTyping( input.value.substring( insertIndex )); // watch user typing	
-			} else {
-				input = ev.target;
-				if( ev.data === "#" ){
-					start( ev.target.selectionStart );
-				}
+				
+				quickTags.userTyping( input.value.substring( insertIndex )); // watching user typing
+			
+			} else if( ev.data === "#" ){
+				
+				startTagging( ev.target.selectionStart ); // trigger 'tagging'
 			}
 		};
 		
 		// reveal
 		return { 
 			watch, 
-			ready, 
-			useSelectedTag, 
-			userClicksTag, 
+			cancelTagging, 
+			insertTag, 
+			userClicksFlag,
 		};
 		
 	})();
@@ -265,12 +304,19 @@
 	Events
 	*/
 	
-	// my custom event for input
+	// custom event delegation for input
 	document.addEventListener('input', inputController.watch, { capture:true });
 	
-	// Event Delegation
-	bj.userDown('.oe-qtags', inputController.ready );
-	bj.userDown('.oe-qtags .qtag', inputController.userClicksTag );
+	// Common Event Delegation
+	bj.userDown('.oe-qtags', inputController.cancelTagging );
+	bj.userDown('.oe-qtags-flag', inputController.userClicksFlag );
+	
+	// clicking on .qtag in .oe-qtags wrapper only:
+	bj.userDown('.oe-qtags .qtag', ( ev ) => {
+		const tagStr = ev.target.textContent;
+		inputController.insertTag( tagStr.substring(1));
+	});
+	
 	
 	
 })( bluejay ); 
