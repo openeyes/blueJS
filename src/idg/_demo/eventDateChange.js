cde4e40dd69bc669bc5c0b5c4070022921eb81f5
Toby Fisher <toby@knowego.com>
2020-10-09 11:55:13 +0100

    Event Date change demo updated

--- src/idg/_demo/eventDateChange.js
+++ src/idg/_demo/eventDateChange.js
@@ -8,22 +8,13 @@
 		let icon = ev.target;
 		let input = ev.target.parentNode.querySelector('input');
 		let text = ev.target.parentNode.querySelector('.js-event-date');
-		
-		if( icon.classList.contains('rewind')){
-			icon.classList.remove('rewind');
-			icon.classList.add('save');
-			bj.hide( text );
-			bj.show( input );
-			setTimeout(() => input.focus(), 20);
-		} else {
-			icon.classList.add('rewind');
-			icon.classList.remove('save');
-			text.textContent = input.value;
-			bj.show( text );
-			bj.hide( input );
-		}
+			
+		icon.classList.add('disabled');
+		bj.hide( text );
+		bj.show( input );
+		setTimeout(() => input.focus(), 20);
 	};
 	
-	bj.userDown('.js-event-date-change > .oe-i', changeEventDate ); 	
+	bj.userDown('.js-event-date-change > .rewind', changeEventDate ); 	
 			
 })( bluejay ); 
