0733f4114caa221b139cb46b7e7d117ec817b0ab
Toby Fisher <toby@knowego.com>
2021-01-08 18:54:02 +0000

    Updated QuickView for SEM Events

--- idg-dev/oe-bluejay.js
+++ idg-dev/oe-bluejay.js
@@ -12102,7 +12102,11 @@ Updated to Vanilla JS for IDG
 									|- .event-date
 									|- .tag
 	
-	Event sidebar only exists on SEM pages
+	DOM data attributes:
+	<li> -|- data-id = "0000001" // OE (this repeated on <a> child)
+		  |- data-bj = complex JSON (combines all OE seperate data attribures into 1)
+	
+	Event sidebar only exists on SEM pages...
 	*/
 	if( document.querySelector('.sidebar-eventlist') === null ) return;
 
@@ -12112,29 +12116,51 @@ Updated to Vanilla JS for IDG
 	*/
 	
 	/**
-	* Quicklook, DOM is hidden.
+	* Quicklook, the little tooltip underneath <li>, in the DOM but hidden.
 	*/
 	const quicklook = {
 		el:null, 
 		show( eventType ){
-			this.el = eventType.parentNode.previousSibling; // unless DOM structure changes
+			// !! relies on the current DOM structure
+			this.el = eventType.parentNode.previousSibling; 
 			this.el.classList.add('fade-in');
 		},
 		hide(){
 			this.el.classList.remove('fade-in');
 		}
 	};
-	
 
 	/**
-	* Quick / Lightning View
-	* DOM built dymnamically and content is loaded from PHP
+	* Quickview, big popup preview.
+	* DOM struture is built dymnamically and content is loaded from an IDG PHP
+	* This is to demo loading.
 	*/
-	const lightningView = {
-		locked:false,
+	const quickview = {
+		
+		id:null,
 		div:null,
-		xhrToken:null,
-		php:null,
+		locked:false,
+		iCloseBtn: 'i-btn-close',
+		
+		/*
+		primary action (because of touch)
+		*/
+		click( json ){
+			if( json.id == this.id ){
+				// Quick view is open...
+				if( this.locked ){
+					this.unlock();
+				} else {
+					this.lock();
+				}
+			} else {
+				// touch device (no "hover")
+				this.show( json );	
+				this.lock();
+			}
+		},
+		
+		// mouse/track pad UX enhancements
 		over( json ){
 			if( this.locked ) return; // ignore
 			this.show( json );
@@ -12143,102 +12169,142 @@ Updated to Vanilla JS for IDG
 			if( this.locked ) return; // ignore
 			this.remove();
 		},
-		click( json ){
-			if( json.php == this.php && this.locked ){
-				this.locked = false;
-				this.div.querySelector('.close-icon-btn').remove();
-				return;
-			}
+		
+		lock(){
 			this.locked = true;
-			this.show( json );
-			
-			const closeBtn = bj.div('close-icon-btn');
-			closeBtn.innerHTML = '<i class="oe-i remove-circle"></i>';
-			this.div.appendChild( closeBtn );
+			this.div.appendChild( bj.div(`${this.iCloseBtn}`));
 		},
+		unlock(){
+			this.locked = false;
+			this.div.querySelector(`.${this.iCloseBtn}`).remove();
+		},
+		
+		// User clicks on the close icon button
 		close(){
 			this.remove();
 			this.locked = false;	
 		},
+		
+		// remove DOM
+		remove(){
+			// this.div.classList.add('fade-out'); // CSS fade-out animation lasts 0.2s
+			this.div.remove();
+			// reset
+			this.div = null;
+			this.id = null;
+		},
+		
+		/**
+		Show the QuickView content
+		There should only be one QuickView open at anytime
+		*/
 		show( json ){
-			// there is only ever one lightning view at a time
 			if( this.div !== null ) this.remove();
+	
+			this.id = json.id;
 			
 			const template =  [
 				'<div class="event-icon"><i class="oe-i-e large {{icon}}"></i></div>',
-				'<div class="lightning-icon"></div>',
-				'<div class="title-date">{{title}} - {{humandate}}</div>',
-				'<div class="quick-view-content"></div>'
+				'<div class="title">{{type}} - {{date}}</div>',
+				'<div class="quick-view-content {{idgQuickView.type}}"></div>'
 			].join('');
 			
-			this.div = bj.div('oe-event-quick-view fade-in');
+			this.div = bj.div('oe-event-quickview fade-in');
 			this.div.innerHTML = Mustache.render( template, json );
 			
-			/*
-			xhr, returns a promise, but check user hasn't moved on to another event icon
-			by comparing tokens
-			*/
-			this.php = json.php;
-			this.xhrToken = bj.getToken();
-			bj.xhr('/idg-php/v3/_load/sidebar/quick-view/' + json.php, this.xhrToken )
-				.then( xreq => {
-					if( this.xhrToken != xreq.token ) return;
-					this.div.querySelector('.quick-view-content').innerHTML = xreq.html;
-				})
-				.catch(e => console.log('PHP failed to load',e));
-			
+			this.load( json.idgQuickView );
+
 			// append div, and wait to load PHP content	
 			document.body.appendChild( this.div );
 		},
-		remove(){
-			this.div.classList.add('fade-out'); // CSS fade-out animation lasts 0.2s
-			this.div.remove();
-			this.div = null;
-			this.php = null;
+		
+		/**
+		Load the QuickView content
+		*/
+		load( qv ){
+			const contentDiv = this.div.querySelector('.quick-view-content');
+			switch( qv.type ){
+				case 'img': 
+					contentDiv.innerHTML = `<img src="${qv.content}" />`;
+				break;
+				case 'pdf': 
+					contentDiv.innerHTML = `<embed src="${qv.content}" width="100%" height="100%"></embed>`;
+				break;
+				case 'php': 
+					bj.xhr( qv.content, this.id )
+						.then( xreq => {
+							// check user hasn't move on whilst we were loading in
+							if( this.id != xreq.token ) return; 
+							contentDiv.innerHTML = xreq.html;
+						})
+						.catch(e => console.log('PHP failed to load',e));
+				break;
+				
+				// just to check the old DOM is still supported by the CSS
+				case 'v3': 
+					this.v3DOM( qv.content );
+				break;
+				
+				default: bj.log('QuickView Error - load content type unknown:' + qv.type);
+			}
+		},
+		v3DOM( src ){
+			const oldDOM = [
+				'<div class="event-quickview">',
+				'<div class="quickview-details">',
+				'<div class="event-icon"><i class="oe-i-e large i-CiExamination"></i></div>', 
+				'<div class="event-date"></div>', // not used!
+				'</div>', // .quickview-details
+				'<div class="quickview-screenshots">',
+				'<img class="js-quickview-image" src="{{imgSrc}}">',
+				'</div>', // .quickview-screenshots
+				'</div>', // .event-quickviews	
+			].join('');
+			this.div.innerHTML = Mustache.render( oldDOM, { imgSrc: src });
 		}
 	};
 	
-	// create a way of reviewing a Quickvew. 
-	const testTarget = document.querySelector('.js-idg-sidebar-demo-quickview');
-	if( testTarget !== null ){
-		lightningView.show( JSON.parse( testTarget.dataset.quickview ));
-	}
-	
 	/*
-	Events 
+	Event delegation
 	*/
 	bj.userEnter('.event .event-type', (ev) => {	
 		quicklook.show( ev.target );
-		lightningView.over( JSON.parse( ev.target.dataset.quickview ));	
+		// quick view JSON is held in <li> parent
+		const li = bj.getParent( ev.target, 'li.event' );
+		quickview.over( JSON.parse( li.dataset.event ));	
 	});	
 																				
 	bj.userLeave('.event .event-type', (ev) => {
 		quicklook.hide(); 
-		lightningView.out();	
+		quickview.out();	
 	});
 	
-	bj.userDown('.oe-event-quick-view .close-icon-btn', () => {
-		lightningView.close();
+	bj.userDown(`.oe-event-quickview .${quickview.iCloseBtn}`, () => {
+		quickview.close();
 	});
 	
 	/*
-	No click events?! Why?
-	Event sidebar is a list of <a> links, historically (and semantically)
-	they  are simply the way to navigate through the Events. Quicklook popup was
-	added later as a desktop (hover) enhancement. Then QuickView was added 
-	but it should STILL be only a hover enhancement (at least for now on IDG).
-	
-	If 'click' to lock OR touch support is required this will handle default <a> click:
+	Intercept "click" on <a> if it's on the .event-type area! 	
 	*/
 	document.addEventListener('click',( ev ) => {
 		if( ev.target.matches('.event .event-type')){
 			ev.preventDefault();
 			ev.stopImmediatePropagation();
-			lightningView.click( JSON.parse( ev.target.dataset.quickview ));
+			// quick view JSON is held in <li> parent
+			const li = bj.getParent( ev.target, 'li.event' );
+			quickview.click( JSON.parse( li.dataset.event ));
 		}
 	},{ capture:true });
 	
-
+	/*
+	Notes on "Click": 
+	Event sidebar is a list of <a> links, historically (and semantically)
+	they were simply the way to navigate through the Events. Then Quicklook popup was
+	added, as a desktop (hover) enhancement, and then QuickView was added.
+	After these enhancements the DOM should be changed so that <a> doesn't wrap the 
+	Quicklook and QuickView DOM and only wraps the link part. This would make it much easier
+	on Touch devices, but this is unlikely to be done at the moment.
+	*/
 	
 })( bluejay ); 
 (function( bj ){
