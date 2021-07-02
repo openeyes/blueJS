3c19f5c7fb22edb8b55ba86e1e3810f0b2198e99
Toby Fisher <toby@knowego.com>
2021-07-02 16:17:27 +0100

    UIX demo of checkbox enabling an input area

--- idg-dev/idg-bluejay.js
+++ idg-dev/idg-bluejay.js
@@ -16224,6 +16224,55 @@ find list ID: 	"add-to-{uniqueID}-list{n}";
 	'use strict';
 
 	/*
+	Quick demo
+	When user ticks checkbox OR
+	When a user toggles a radio
+	Enable and focus on textarea
+	*/
+	
+	if( document.querySelector('.js-idg-enable-textarea') == null ) return;
+	
+	const map = new Map();
+	
+	document.querySelectorAll('.js-idg-enable-textarea').forEach( wrap => {
+		const idg = JSON.parse( wrap.dataset.idg );
+		map.set( idg.input, idg.textarea ); // textarea is an array (could be 2)		
+	});
+	
+	// listen for changes
+	document.addEventListener('change', ev => {
+		const input = ev.target;
+		if(! map.has( input.name )) return; // ignore 
+		
+		// should the textarea be active?
+		let activate = false;
+		if( input.type == "radio" && input.value == "1" ) activate = true;
+		if( input.type == "checkbox" && input.checked ) activate = true;
+		
+		// array 
+		map.get( input.name ).forEach(( textareaID, index ) => {
+			const textarea = document.getElementById(textareaID);
+			
+			if( activate ){
+				textarea.disabled = false;
+				if( index == 0) textarea.focus();
+			} else {
+				textarea.value = "";
+				textarea.style.height = "";
+				textarea.disabled = true;
+			}
+		});
+	});
+	
+	
+	
+	
+})( bluejay ); 
+(function( bj ) {
+
+	'use strict';
+
+	/*
 	Demo the expand / collapse 
 	these are also on a demo home hub!
 	*/
